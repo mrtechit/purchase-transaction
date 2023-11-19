f781190db00264e4d2db19f3f79bdc2463baa977
mrtechit <anirudhsinghforever@gmail.com>
2023-11-19 20:46:51 +1100

    add gorm

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 func main() {
 
+}
+
+func connectToPostgreSQL() (*gorm.DB, error) {
+	dsn := "user=postgres password=postgres dbname=postgres host=localhost port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
