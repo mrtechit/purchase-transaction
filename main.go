4631cb5bc887c725342501876c41843793326133
mrtechit <anirudhsinghforever@gmail.com>
2023-11-20 03:16:02 +1100

    add env file

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/mrtechit/purchase-transaction/db"
 	"github.com/mrtechit/purchase-transaction/httpserver"
 	"github.com/mrtechit/purchase-transaction/model"
@@ -8,10 +10,17 @@ import (
 	"gorm.io/gorm"
 	"log"
 	"net/http"
+	"os"
+	"path"
 )
 
 func main() {
 
+	configDir := os.Getenv("CONFIG_DIR")
+
+	if err := godotenv.Load(path.Join(configDir, ".env")); err != nil {
+		fmt.Println(".env file will not be used")
+	}
 	gormDb, err := migrateAndConnectToPsg()
 	if err != nil {
 		log.Fatal("[main] error init database")
@@ -28,7 +37,7 @@ func main() {
 }
 
 func conntectToPsg() (*gorm.DB, error) {
-	dsn := "user=postgres password=postgres dbname=postgres host=localhost port=5432 sslmode=disable"
+	dsn := os.Getenv("POSTGRES_DSN")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
